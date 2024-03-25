3a79cca2021f42fc6057afbbaebdfb815310b65d
isihyeon <tlgus0113@gmail.com>
2024-03-25 22:13:03 +0900

    [Feat] LoginScreen 생성 - 폴더 구조 정의

--- lib/common/component/custom_text_form_field.dart
+++ lib/common/component/custom_text_form_field.dart
@@ -42,6 +42,7 @@ class CustomTextFormField extends StatelessWidget {
         filled: true,
         // 모든 Input 상태의 기본 세팅
         border: baseBorder,
+        enabledBorder: baseBorder,
         // 선택 시 Input의 세팅
         focusedBorder: baseBorder.copyWith(
           borderSide: baseBorder.borderSide.copyWith(color: PRIMARY_COLOR),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_codefactory_practice_app/common/component/custom_text_form_field.dart';
+import 'package:flutter_codefactory_practice_app/user/view/login_screen.dart';
 
 void main() {
   runApp(
@@ -13,23 +13,8 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomTextFormField(
-                hintText: '이메일을 입력해 주세요',
-                onChanged: (String value) {},
-              ),
-              CustomTextFormField(
-                hintText: '비밀번호를 입력해 주세요',
-                onChanged: (String value) {},
-                obscureText: true,
-              ),
-            ],
-          ),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
   }
 }
